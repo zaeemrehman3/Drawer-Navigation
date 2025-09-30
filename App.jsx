67f384f45d159b6c627c7905359c8f9c92c9f5d6
Zaeem Rehman <zaeemrehman39@gmail.com>
2025-09-30 12:43:01 +0500

    screens

--- App.jsx
+++ App.jsx
@@ -1,11 +1,62 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 const App = () => {
-  return (
+
+  const Drawer = createDrawerNavigator();
+
+  const HomeScreen = ( { navigation } ) => {
     <View>
-      <Text>App</Text>
+      <Text>Home Screen</Text>
+      <Button title='Open Drawer' onPress={()=> navigation.openDrawer()} />
     </View>
+  };
+
+  
+  const ProfileScreen = ( { navigation } ) => {
+    <View>
+      <Text>Profile Screen</Text>
+      <Button title='Open Drawer' onPress={()=> navigation.openDrawer()} />
+    </View>
+  };
+
+  
+  const SettingScreen = ( { navigation } ) => {
+    <View>
+      <Text>Setting Screen</Text>
+      <Button title='Open Drawer' onPress={()=> navigation.openDrawer()} />
+    </View>
+  };
+
+
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+      screenOptions={{
+        drawerStyle : {
+          backgroundColor : '#e6e6e6',
+          width : 240,
+        },
+        drawerLabelStyle : {
+          fontSize : 18,
+          color : '#333',
+        },
+        headerStyle : {
+          backgroundColor : '#6200EE',
+        },
+        headerTintColor : '#fff',
+        headerTitleAlign : 'center',
+      }}
+      >
+        <Drawer.Screen name='Home'component={HomeScreen} />
+        <Drawer.Screen name='Profile' component={ProfileScreen} />
+        <Drawer.Screen name='Settings' component={SettingScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
